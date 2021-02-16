58afc89eecb3b0ca537a38d64f2ea0315bbb39e8
Sasitha Asaranga <sasithaasaranga@gmail.com>
2021-02-16 14:44:33 +0530

    Update fileChooser.js

--- www/fileChooser.js
+++ www/fileChooser.js
@@ -1,6 +1,6 @@
 module.exports = {
     open: function (filter, success, failure) {
-        console.log('opened filechooser');
+        console.log('opened filechooser 2');
         if (typeof filter === 'function') {
             failure = success;
             success = filter;
